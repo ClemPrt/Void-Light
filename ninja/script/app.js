9fe6cd40051d646d19a20583909a79af59b93a6c
ClemPrt <clement.parot@hetic.net>
2018-04-11 01:34:43 +0200

    Again
    again

--- ninja/script/app.js
+++ ninja/script/app.js
@@ -1,7 +1,7 @@
-let population = 100
-let food = 75
-let water = 75
-let energy = 50
+let population = 198
+let food = 100
+let water = 100
+let energy = 100
 let hygiene = 100
 let oxygen = 100
 let comfort = 50
@@ -9,62 +9,61 @@ let humor = 75
 let min = 25
 let medium = 50
 let max = 75
-let bitcoins = 25
+let bitcoins = 100
 let animate = document.querySelectorAll('.animate')
 let moreFood = document.querySelector('.moreFood')
 let moreWater = document.querySelector('.moreWater')
 let moreOxygen = document.querySelector('.moreOxygen')
-let moreComfort = document.querySelector('.moreComfort')
 let moreEnergy = document.querySelector('.moreEnergy')
 let moreHygiene = document.querySelector('.moreHygiene')
+let moreComfort = document.querySelector('.moreComfort')
 
 let timer = setInterval(
   function(){
-    myLife()
     myPopulation()
     myFood()
     myWater()
     myOxygen()
-    myHumor()
-    myComfort()
     myEnergy()
     myHygiene()
+    myComfort()
+    myHumor()
   },700
 )
 
 
-function myLife() {
-  if ((food || water) < min){
-    population -= 1 }
-  if ((comfort || hygiene || energy || oxygen) < min){
-  humor -= 1 }
-  if ((energy || hygiene || oxygen) < min){
-    comfort-1}
-}
+
 
 
 function myPopulation() {
   document.querySelector('#population').innerHTML = Math.floor(`${population}`)
-
-  if(population < 1){
+  if (population < 1) {
     let absolute = document.querySelector('.absolute').innerHTML = "PERDU"
-  } else if((food || water) > 100){
-    population +=1
+  } else if (population >= 200) {
+    absolute = document.querySelector('.absolute').innerHTML = "BRAVO"
+  } else if ((food || water) > 100) {
+    population += 0.3
+    console.log("GOOD")
   } else if ((food || water) > 150) {
-    console.log("150")
+    population += 3
+    console.log("ALLEZ")
   } else if ((food || water) > 175) {
+    population += 5
     console.log("175")
   } else if ((food || water) > 200) {
+    population += 10
     console.log("200")
   }
 
 }
 
 function myFood() {
-  food = document.querySelector('#food').innerHTML = Math.floor(food -= population / 100)
+  document.querySelector('#food').innerHTML = Math.floor(food -= population / 100)
 
   if ((food) < 1){
     food = 0
+  } else if (food < 25){
+    population -= 1
   }
 }
 
@@ -73,6 +72,8 @@ function myWater() {
 
   if ((water) < 1){
     water = 0
+  } else if (water < 25) {
+    population -= 1
   }
 }
 
@@ -81,19 +82,25 @@ function myOxygen() {
 
   if ((oxygen) < 1){
     oxygen = 0
+  } else if (oxygen < 15){
+    population -= 10
   }
 }
 
 function myHumor() {
-  document.querySelector('#humor').innerHTML = Math.floor(humor -= population / 100)
+  document.querySelector('#humor').innerHTML = Math.floor(humor)
 
   if ((humor) < 1){
     humor = 0
+  } else if ((food && water && oxygen && hygiene) > 75){
+    humor += 0.1
+  } else {
+    humor -= 10
   }
 }
 
 function myComfort() {
-  document.querySelector('#comfort').innerHTML = Math.floor(comfort -= population / 200)
+  document.querySelector('#comfort').innerHTML = Math.floor(comfort -= (population/4) / 200)
 
   if ((comfort) < 1){
     comfort = 0
@@ -105,72 +112,72 @@ function myEnergy() {
 
 if ((energy) < 1){
   energy = 1
+} else if (energy < 25){
+  food -= 1, water -= 1, oxygen -= 5
 }
 }
 
 function myHygiene() {
-  document.querySelector('#hygiene').innerHTML = Math.floor(hygiene -= population / 100)
+  document.querySelector('#hygiene').innerHTML = Math.floor(hygiene -= (population/4) / 100)
 
   if ((hygiene) < 1){
     hygiene = 0
   }
 }
 
-/* function myBitcoins() {
-  let changeFood = 1
-  let changeWater = 2
-  let choice.addEventListener('click', function()){
-    choice = dataset.value
-    if
 
-  }
-}
 
-*/
 
 
 
 moreFood.addEventListener(
   "click",
   function(){
-    food += 10
+    food += 30
+    bitcoins -= 5
   }
 )
 
 moreWater.addEventListener(
   "click",
   function () {
-    water += 10
+    water += 30
+    bitcoins -= 5
   }
 )
 
 moreOxygen.addEventListener(
   "click",
   function () {
-    oxygen += 10
+    oxygen += 30
+    bitcoins -= 5
   }
 )
 
 moreComfort.addEventListener(
   "click",
   function () {
-    comfort += 10
+    comfort += 30
+    bitcoins -= 5
   }
 )
 
 moreEnergy.addEventListener(
   "click",
   function () {
-    energy += 10
+    energy += 30
+    bitcoins -= 5
   }
 )
 
 moreHygiene.addEventListener(
   "click",
   function () {
-    hygiene += 10
+    hygiene += 30
+    bitcoins -= 5
   }
 )
+
 
 
 for(let i = 0; i < animate.length; i++){
@@ -216,3 +223,4 @@ for (let i = 0; i < thumbnail.length; i++) {
 function loose(){
 
 }
+
